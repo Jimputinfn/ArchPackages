5ec150654e1befad08a36737dec0a7e40eba25a9
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-01-28 13:59:47 +0000

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,9 @@
+
+echo Warning this is NOT arch install script this only install applications that i use in my install.
+echo Warning Script is NOT tested yet...
+sudo pacman -S flatpak steam  Discord kitty openrgb neofetch htop neovim
+su
+flatpak install flathub one.ablaze.floorp
+reboot
+
+# My first Linux project so there might be some things wrong
