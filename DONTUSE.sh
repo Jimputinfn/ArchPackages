25d4ffb1c262d7d6ede5dbf817b1292af35d857b
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-01-30 13:45:02 +0000

    Update DONTUSE.sh

--- DONTUSE.sh
+++ DONTUSE.sh
@@ -1,8 +1,15 @@
 #Do not use this script yet its under development
+                   _       _____           _                         
+echo    /\            | |     |  __ \         | |                        
+echo   /  \   _ __ ___| |__   | |__) |_ _  ___| | ____ _  __ _  ___  ___ 
+echo  / /\ \ | '__/ __| '_ \  |  ___/ _` |/ __| |/ / _` |/ _` |/ _ \/ __|
+echo / ____ \| | | (__| | | | | |  | (_| | (__|   < (_| | (_| |  __/\__ \
+echo /_/    \_\_|  \___|_| |_| |_|   \__,_|\___|_|\_\__,_|\__, |\___||___/
+                                                      __/ |          
+                                                     |___/           
 
 
-
-sudo pacman -S flatpak neovim htop neofetch openrgb kitty discord steam firefox git plasma-desktop
+sudo pacman -S flatpak neovim htop neofetch openrgb kitty discord steam firefox git plasma-desktop linux-lts
 
 
 
