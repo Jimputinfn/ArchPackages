4da8df349146e39b109457b3f8ee86ebfc5dcd70
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-01-31 22:14:05 +0200

    Update ArchPackages.sh

--- ArchPackages.sh
+++ ArchPackages.sh
@@ -2,12 +2,12 @@
 
 echo -ne "
 -------------------------------------------------------------------------
-                    Automated Arch Linux Package Installer V2
+                    Automated Arch Linux Package Installer V3
 -------------------------------------------------------------------------
                Made By Jimputin
 This isnt arch install script this installs applications and desktop that i use.
 "
 
 
-sudo pacman -S flatpak neovim htop neofetch openrgb kitty discord steam firefox git plasma-desktop linux-lts
+sudo yay -S flatpak neovim htop neofetch openrgb kitty discord steam firefox git plasma-desktop linux-lts thorium-browser-bin
 grub-mkconfig -o /boot/grub/grub.cfg
