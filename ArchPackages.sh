aaa53e90b0907e0e07f276e4cf749cd578d83f8e
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-01-31 22:14:38 +0200

    Update ArchPackages.sh

--- ArchPackages.sh
+++ ArchPackages.sh
@@ -9,5 +9,4 @@ This isnt arch install script this installs applications and desktop that i use.
 "
 
 
-sudo yay -S flatpak neovim htop neofetch openrgb kitty discord steam firefox git plasma-desktop linux-lts thorium-browser-bin
-grub-mkconfig -o /boot/grub/grub.cfg
+yay -S flatpak neovim htop neofetch openrgb kitty discord steam firefox git plasma-desktop linux-lts thorium-browser-bin
