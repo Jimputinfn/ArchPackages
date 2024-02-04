9dc8601c22a660bb2c10283cd0903ec2c8fd3fa2
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-02-04 22:18:00 +0200

    Update ArchPackages.sh

--- ArchPackages.sh
+++ ArchPackages.sh
@@ -10,3 +10,5 @@ This isnt arch install script this installs applications and desktop that i use.
 
 
 yay -S flatpak neovim htop neofetch openrgb kitty discord steam firefox git plasma-desktop linux-lts thorium-browser-bin grub-customizer i3-wm
+
+echo Done!
